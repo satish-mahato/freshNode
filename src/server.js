8059a91afc686f35066954b8fd80aa295f0b312d
satish7654 <satishsingh202dd+@gmail.com>
2025-07-14 21:49:00 +0545

    update

--- src/server.js
+++ src/server.js
@@ -7,6 +7,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 app.use(express.json()), app.use(express.urlencoded({ extended: true }));
+app.use("/hello", (req, res) => {
+  res.send("Hello World");})
 app.use("/api", apiRoutes);
 app.use(
   "/files",
